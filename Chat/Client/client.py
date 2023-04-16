4a718b55a7b12eb0d899b4901bfac966f27f2adf
AndreiGosovich <andrei_gosovich@mail.ru>
2023-04-16 09:43:06 +0300

    Lesson 5

--- Chat/Client/client.py
+++ Chat/Client/client.py
@@ -1,11 +1,18 @@
-import json
-import sys
 import calendar
-from socket import *
+import json
+import logging
+import sys
 from datetime import datetime, timezone
+from socket import *
+
+sys.path.append("../log")
+import client_log_config
+
+logger = logging.getLogger('chat.client')
 
 
 def get_arguments():
+    logger.debug('Получаем аргументы')
     args = sys.argv
     if len(args) > 1:
         addr = args[1]
@@ -16,10 +23,12 @@ def get_arguments():
     if len(args) > 2:
         port = int(args[2])
 
+    logger.info(f'address = {addr}, port = {port}')
     return addr, port
 
 
 def create_presence_message(account_name, status='online', _type='status'):
+    logger.debug("Создаём сообщение серверу")
     return json.dumps({
         "action": "presence",
         "time":  calendar.timegm(datetime.now(timezone.utc).utctimetuple()),
@@ -32,18 +41,21 @@ def create_presence_message(account_name, status='online', _type='status'):
 
 
 def parse_server_response(data):
+    logger.debug("Парсим ответ сервера")
     response_data = json.loads(data)
 
     return response_data
 
 
 def get_socket(addr_family, socket_type):
+    logger.debug("Создаём сокет")
     s = socket(addr_family, socket_type)  # Создать сокет TCP
 
     return s
 
 
 def run_chat_client(addr='', port=7777):
+    logger.debug("Запускаем клиент")
     if not addr:
         addr = ''
     if not port:
@@ -54,16 +66,20 @@ def run_chat_client(addr='', port=7777):
 
     msg = create_presence_message('Andrei')
 
+    logger.debug("Отправляем сообщение серверу")
     s.send(msg.encode('utf-8'))
     data = s.recv(1000000)
+    logger.debug("Получили ответ сервера")
 
     server_response = parse_server_response(data.decode('utf-8'))
 
-    print('Сообщение от сервера: ', server_response, ', длиной ', len(data), ' байт')
+    logger.info(f'Сообщение от сервера: {server_response}, длиной {len(data)} байт')
     s.close()
+    logger.debug("Закрываем соект. Работа клиента завершена.")
 
 
 def main():
+    logger.debug("Запуск клиента")
     args = get_arguments()
 
     run_chat_client(args[0], args[1])
@@ -73,4 +89,4 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print(e)
+        logger.critical(e)
